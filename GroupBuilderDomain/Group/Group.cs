7b8deb185b9c56d2046600237fe1132f7e6237e5
akl <akl@ITMDKLT047>
2019-02-19 14:21:39 +0100

    Add project files.

---
+++ GroupBuilderDomain/Group/Group.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupBuilderDomain
+{
+    class Group
+    {
+        ISet<User> Users { get; set; }
+    }
+}
---
+++ GroupBuilderDomain/User/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupBuilderDomain
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string StudentId { get; set; }
+    }
+}
