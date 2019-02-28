b3828acf7d9ab3b243eb8e9a9c4cca4ca1cca636
akl <akl@It-minds.dk>
2019-02-28 10:29:00 +0100

    Error message for randomizer choice

--- GroupBuilderApplication/Room/Commands/RandomizeRoom/RandomizerModel.cs
+++ GroupBuilderApplication/Room/Commands/RandomizeRoom/RandomizerModel.cs
@@ -12,6 +12,8 @@ namespace GroupBuilderApplication.Commands.RandomizeGroups
         public int MaximumGroupSize { get; set; } = 6;
         public int MinimumNumberOfGroups { get; set; } = 1;
         public int MaximumNumberOfGroups { get; set; } = Int32.MaxValue;
+
+        [EnumDataType(typeof(RandomizerEnum), ErrorMessage = "The chosen algorithm is not know to the system. Check spelling")]
         public RandomizerEnum Algorithm { get; set; } = RandomizerEnum.EvenSplit;
 
         
