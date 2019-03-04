f76d78bf4890cdbfb458bff1d45d906cc0a73fe1
akl <akl@It-minds.dk>
2019-03-04 16:02:06 +0100

    dnd

--- GroupBuilder/wwwroot/index.js
+++ GroupBuilder/wwwroot/index.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c72cd5e6117ec252ca09";
+/******/ 	var hotCurrentHash = "24b48ebff84ee4d38932";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
